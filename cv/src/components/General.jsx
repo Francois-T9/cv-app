d1e3caa532f17d1bf35a6a4a598ae4fdb0ebb786
FrancoisT9 <francois.thullier98@gmail.com>
2025-02-25 17:34:58 +0100

    Added use input for first and last name

--- cv/src/components/General.jsx
+++ cv/src/components/General.jsx
@@ -1,10 +1,26 @@
-// import { useState } from "react";
+import { useState } from "react";
 import "../styles/general.css";
 
 function General() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+
+
+const handleFirstNameChange = (e) => {
+   setFirstName(e.target.value);
+}
+const handleLasttNameChange = (e) => {
+  setLastName(e.target.value);
+}
+
+
   return (
     <div className="general">
       <h1>General information</h1>
+
+      <label >First name:<input type="text" value={firstName} onChange={handleFirstNameChange} /></label>
+      <label>Last name: <input type="text" value={lastName} onChange={handleLasttNameChange}/></label>
+      <button>Submit</button>
     </div>
   );
 }
