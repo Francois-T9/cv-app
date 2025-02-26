5067d6b2da46865dd7292d5233a03f901530727b
Francois <francois.thullier98@gmail.com>
2025-02-26 09:31:15 +0100

    Added a form that pops depending on button state

--- cv/src/components/General.jsx
+++ cv/src/components/General.jsx
@@ -5,22 +5,56 @@ function General() {
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
 
+  const handleFirstNameChange = (e) => {
+    setFirstName(e.target.value);
+  };
 
-const handleFirstNameChange = (e) => {
-   setFirstName(e.target.value);
-}
-const handleLasttNameChange = (e) => {
-  setLastName(e.target.value);
-}
-
+  const [toggleForm, setToggleForm] = useState(false);
+  const handleLasttNameChange = (e) => {
+    setLastName(e.target.value);
+  };
+  const popupForm = (e) => {
+    e.preventDefault();
+    setToggleForm(!toggleForm);
+  };
 
   return (
     <div className="general">
-      <h1>General information</h1>
+      <button onClick={popupForm} className="open-form-btn">
+        Open form
+      </button>
 
-      <label >First name:<input type="text" value={firstName} onChange={handleFirstNameChange} /></label>
-      <label>Last name: <input type="text" value={lastName} onChange={handleLasttNameChange}/></label>
-      <button>Submit</button>
+      {toggleForm && (
+        <div className="general-info-form">
+          <form action="">
+            <label htmlFor="">
+              First name{" "}
+              <input
+                type="text"
+                value={firstName}
+                onChange={handleFirstNameChange}
+              />
+            </label>
+            <label htmlFor="">
+              Last name :{" "}
+              <input
+                type="text"
+                value={lastName}
+                onChange={handleLasttNameChange}
+              />
+            </label>
+            <button onClick={popupForm}>Submit</button>
+            <button onClick={popupForm}>Close form</button>
+          </form>
+        </div>
+      )}
+
+      <div className="display-general-info">
+        <h2>Full name</h2>
+        <p>
+          {firstName} {lastName}
+        </p>
+      </div>
     </div>
   );
 }
