401f3be602dcacea543fd7d03c49105bd9d433d2
Francois <francois.thullier98@gmail.com>
2025-02-27 09:52:58 +0100

    Added a function to display information on button click

--- cv/src/components/General.jsx
+++ cv/src/components/General.jsx
@@ -4,18 +4,40 @@ import "../styles/general.css";
 function General() {
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [toggleForm, setToggleForm] = useState(false);
+
+  const [displayName, setDisplayName] = useState("");
+  const [displayEmail, setDisplayEmail] = useState("");
+  const [displayPhone, setDisplayPhone] = useState("");
 
   const handleFirstNameChange = (e) => {
     setFirstName(e.target.value);
   };
 
-  const [toggleForm, setToggleForm] = useState(false);
   const handleLasttNameChange = (e) => {
     setLastName(e.target.value);
   };
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePhoneChange = (e) => {
+    setPhone(e.target.value);
+  };
+
   const popupForm = (e) => {
     e.preventDefault();
     setToggleForm(!toggleForm);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setDisplayName(firstName + " " + lastName);
+    setDisplayEmail(email);
+    setDisplayPhone(phone);
   };
 
   return (
@@ -43,18 +65,25 @@ function General() {
                 onChange={handleLasttNameChange}
               />
             </label>
-            <button onClick={popupForm}>Submit</button>
+            <label htmlFor="">
+              Email :{" "}
+              <input type="email" value={email} onChange={handleEmailChange} />
+            </label>
+            <label htmlFor="">
+              Phone :{" "}
+              <input type="tel" value={phone} onChange={handlePhoneChange} />
+            </label>
+            <button onClick={handleSubmit}>Submit</button>
             <button onClick={popupForm}>Close form</button>
           </form>
         </div>
       )}
-
-      <div className="display-general-info">
-        <h2>Full name</h2>
-        <p>
-          {firstName} {lastName}
-        </p>
-      </div>
+      <h2>Full name</h2>
+      <p>{displayName}</p>
+      <h2>Email</h2>
+      <p>{displayEmail}</p>
+      <h2>Phone</h2>
+      <p>{displayPhone}</p>
     </div>
   );
 }
