9bc34a83bf4307a4683c01bc08b313eefdb55f55
Francois <francois.thullier98@gmail.com>
2025-03-06 09:25:16 +0100

    Reorganized form input fields as an object

--- cv/src/components/General.jsx
+++ cv/src/components/General.jsx
@@ -2,46 +2,52 @@ import { useState } from "react";
 import "../styles/general.css";
 
 function General() {
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [email, setEmail] = useState("");
-  const [phone, setPhone] = useState("");
+  // Object containing the state of input fields of the form
+  const [generalForm, setGeneralForm] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    phone: "",
+  });
+
+  // Function toggling the update of the form input fields
+  const handleFormChange = (e) => {
+    const { name, value } = e.target;
+    setGeneralForm((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  // Function to open or close the form
   const [toggleForm, setToggleForm] = useState(false);
-
-  const [displayName, setDisplayName] = useState("");
-  const [displayEmail, setDisplayEmail] = useState("");
-  const [displayPhone, setDisplayPhone] = useState("");
-
-  const handleFirstNameChange = (e) => {
-    setFirstName(e.target.value);
-  };
-
-  const handleLasttNameChange = (e) => {
-    setLastName(e.target.value);
-  };
-
-  const handleEmailChange = (e) => {
-    setEmail(e.target.value);
-  };
-
-  const handlePhoneChange = (e) => {
-    setPhone(e.target.value);
-  };
 
   const popupForm = (e) => {
     e.preventDefault();
     setToggleForm(!toggleForm);
   };
 
+  // Displaying object based on user inputs and submit
+  const [displayForm, setDisplayForm] = useState({
+    displayName: "",
+    displayEmail: "",
+    displayPhone: "",
+  });
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    setDisplayName(firstName + " " + lastName);
-    setDisplayEmail(email);
-    setDisplayPhone(phone);
+
+    setDisplayForm((prevState) => ({
+      ...prevState,
+      displayName: generalForm.firstName + " " + generalForm.lastName,
+      displayEmail: generalForm.email,
+      displayPhone: generalForm.phone,
+    }));
   };
 
   return (
     <div className="general">
+      <h1>General information</h1>
       <button onClick={popupForm} className="open-form-btn">
         Open form
       </button>
@@ -53,37 +59,50 @@ function General() {
               First name{" "}
               <input
                 type="text"
-                value={firstName}
-                onChange={handleFirstNameChange}
+                name="firstName"
+                value={generalForm.firstName}
+                onChange={handleFormChange}
               />
             </label>
             <label htmlFor="">
               Last name :{" "}
               <input
                 type="text"
-                value={lastName}
-                onChange={handleLasttNameChange}
+                name="lastName"
+                value={generalForm.lastName}
+                onChange={handleFormChange}
               />
             </label>
             <label htmlFor="">
               Email :{" "}
-              <input type="email" value={email} onChange={handleEmailChange} />
+              <input
+                type="email"
+                value={generalForm.email}
+                onChange={handleFormChange}
+                name="email"
+              />
             </label>
             <label htmlFor="">
               Phone :{" "}
-              <input type="tel" value={phone} onChange={handlePhoneChange} />
+              <input
+                type="tel"
+                value={generalForm.phone}
+                onChange={handleFormChange}
+                name="phone"
+              />
             </label>
             <button onClick={handleSubmit}>Submit</button>
             <button onClick={popupForm}>Close form</button>
           </form>
         </div>
       )}
+
       <h2>Full name</h2>
-      <p>{displayName}</p>
+      <p>{displayForm.displayName}</p>
       <h2>Email</h2>
-      <p>{displayEmail}</p>
+      <p>{displayForm.displayEmail}</p>
       <h2>Phone</h2>
-      <p>{displayPhone}</p>
+      <p>{displayForm.displayPhone}</p>
     </div>
   );
 }
